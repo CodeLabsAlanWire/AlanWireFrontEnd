7c6926e10fb07634c90901d68dddab37fe331143
Jack Hulen <111541579+Jhulen37@users.noreply.github.com>
2023-05-01 18:48:31 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { AuthGuardComponent } from './shared/auth-guard/auth-guard.component';
+import { EmployeeDocsComponent } from './employee-docs/employee-docs.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent,
+    AuthGuardComponent,
+    EmployeeDocsComponent,
+    NavBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/employee-docs/employee-docs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-docs',
+  templateUrl: './employee-docs.component.html',
+  styleUrls: ['./employee-docs.component.css']
+})
+export class EmployeeDocsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
